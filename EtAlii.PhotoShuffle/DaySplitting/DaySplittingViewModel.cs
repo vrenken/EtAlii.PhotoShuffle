da715a864cb62626fdf347fe47d3703c62edf668
Peter Vrenken <github.com@petervrenken.nl>
2019-09-29 11:45:46 +0200

    Reduced code for day-splitting feature

--- EtAlii.PhotoShuffle/DaySplitting/DaySplittingViewModel.cs
+++ EtAlii.PhotoShuffle/DaySplitting/DaySplittingViewModel.cs
@@ -4,11 +4,10 @@ namespace EtAlii.PhotoShuffle
     using System.Collections.ObjectModel;
     using System.ComponentModel;
     using System.IO;
-    using System.Text;
     using System.Threading.Tasks;
     using Microsoft.WindowsAPICodePack.Dialogs;
     
-    public class DaySplittingViewModel : BindableBase, IErrorHandler
+    public class DaySplittingViewModel : ProcessViewModelBase
     {
         private readonly TimeStampBuilder _timeStampBuilder;
         public string Source { get => _source; set => SetProperty(ref _source, value); }
@@ -22,27 +21,29 @@ namespace EtAlii.PhotoShuffle
 
         public TimeStampSource TimeStampSource { get => _timeStampSource; set => SetProperty(ref _timeStampSource, value); }
         private TimeStampSource _timeStampSource = TimeStampSource.MetaData;
-
-        public DispatcherObservableCollection<string> Output { get; }
-        private readonly ObservableCollection<string> _output;
-        
-        public AsyncCommand TestDaySplitCommand { get; }
-        public AsyncCommand DaySplitCommand { get; }
         public IAsyncCommand SelectSourceCommand { get; }
 
         public DaySplittingViewModel(TimeStampBuilder timeStampBuilder)
         {
             _timeStampBuilder = timeStampBuilder;
             
-            TestDaySplitCommand = new AsyncCommand(() => DaySplit(false), CanDaySplit, this);
-            DaySplitCommand = new AsyncCommand(DaySplit, CanDaySplit, this);
-            
             SelectSourceCommand = new AsyncCommand(() => Select(() => Source, value => Source = value));
 
             PropertyChanged += OnPropertyChanged;
-            
-            _output = new ObservableCollection<string>();
-            Output = new DispatcherObservableCollection<string>(_output);
+        }
+
+        protected override bool CanExecute()
+        {
+            var prerequisitesMet = 
+                ! string.IsNullOrWhiteSpace(Source) &
+                Directory.Exists(Source);
+            return prerequisitesMet;
+        }
+
+        protected override Task ExecuteAsync(bool commit, ObservableCollection<string> output)
+        {
+            var process = new DaySplittingProcess(_timeStampBuilder);
+            return process.Execute(Source, output, AddMonthToFolderName, AddYearToFolderName, TimeStampSource, commit);
         }
 
         private void OnPropertyChanged(object sender, PropertyChangedEventArgs e)
@@ -50,18 +51,10 @@ namespace EtAlii.PhotoShuffle
             switch (e.PropertyName)
             {
                 case nameof(Source):
-                    DaySplitCommand.RaiseCanExecuteChanged();
-                    TestDaySplitCommand.RaiseCanExecuteChanged();
+                    ExecuteCommand.RaiseCanExecuteChanged();
+                    TestCommand.RaiseCanExecuteChanged();
                     break;
             }
-        }
-
-        public void HandleError(Exception ex)
-        {
-            var sb = new StringBuilder();
-            sb.AppendLine(ex.Message);                    
-            sb.AppendLine(ex.StackTrace);                    
-            _output.Add(sb.ToString());
         }
 
         private Task Select(Func<string> getter, Action<string> setter)
@@ -77,28 +70,6 @@ namespace EtAlii.PhotoShuffle
                 setter(dialog.FileName);
             }
             return Task.CompletedTask;
-        }
-        
-        private Task DaySplit()
-        {
-            return DaySplit(true);
-        }
-
-        private async Task DaySplit(bool commit)
-        {
-//            return Task.Run(() =>
-//            {
-                var process = new DaySplittingProcess(_timeStampBuilder);
-                await process.Execute(Source, _output, AddMonthToFolderName, AddYearToFolderName, TimeStampSource, commit);
-//            });
-        }
-
-        private bool CanDaySplit()
-        {
-            var prerequisitesMet = 
-                ! string.IsNullOrWhiteSpace(Source) &
-                Directory.Exists(Source);
-            return prerequisitesMet;
         }
     }
 }
